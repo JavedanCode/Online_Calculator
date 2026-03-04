4c6c04176733da3078a4262d6b6850a746596446
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-03-04 17:45:35 +0300

    Implement simple calculator logic + add numbers and operator buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,18 @@
 //Variables
-const PLUS = 0;
-const MINUS = 1;
-const MULTIPLICATION = 2;
-const DIVISION = 3;
-
-//Arrays
-let operatorArray = ["+", "-", "*", "/"];
+let justCalculated = false;
 
 //Objects
 let expression = {
-  left: 0,
+  left: "",
   operator: "",
-  right: 0,
+  right: "",
 };
 
 //DOM Selections
 let operation = document.querySelector(".screen");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let textArea = document.querySelector("textarea");
 
 //Functions
 function add(a, b) {
@@ -31,16 +28,99 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) return "Error: Cannot divide by 0!";
-
+  if (b === 0) {
+    alert("Error: Cannot divide by 0!");
+    return 0;
+  }
   return a / b;
 }
 
 function operate() {
-  //I want to read the whole string
-  //Put everything on the left of the operator in the left variable
-  //Put everything on the right of the operator in the right variable
-  //I need a way to detect the operator and put it in the operator variable;
-  //Depending on the operator, do the correct calculations
-  //Update the string and return it to the screen text field.
+  if (expression.operator === "+") {
+    expression.left = add(Number(expression.left), Number(expression.right));
+  } else if (expression.operator === "-") {
+    expression.left = subtract(
+      Number(expression.left),
+      Number(expression.right),
+    );
+  } else if (expression.operator === "*") {
+    expression.left = multiply(
+      Number(expression.left),
+      Number(expression.right),
+    );
+  } else if (expression.operator === "/") {
+    expression.left = divide(Number(expression.left), Number(expression.right));
+  }
+  expression.right = "";
 }
+
+function renderScreen() {
+  textArea.value = expression.left + expression.operator + expression.right;
+}
+
+function updateExpression() {
+  const value = this.textContent;
+  if (this.classList.contains("number")) {
+    if (expression.operator === "") {
+      if (justCalculated) {
+        expression.left = value;
+        expression.operator = "";
+        expression.right = "";
+        justCalculated = false;
+      } else {
+        expression.left += value;
+      }
+    } else {
+      expression.right += value;
+    }
+  }
+
+  if (this.classList.contains("operator")) {
+    if (this.classList.contains("equals")) {
+      if (expression.left !== "" && expression.right !== "") {
+        operate();
+
+        justCalculated = true;
+      }
+      expression.operator = "";
+      expression.right = "";
+      renderScreen();
+      return;
+    }
+    if (this.classList.contains("negative")) {
+      if (expression.left === "") {
+        expression.left = "-";
+        console.log(expression);
+        renderScreen();
+        return;
+      } else if (expression.right === "" && expression.operator !== "") {
+        expression.right = "-";
+        renderScreen();
+        return;
+      }
+    }
+    if (expression.left === "") return;
+
+    if (expression.operator === "") {
+      expression.operator = value;
+    } else if (expression.right === "") {
+      expression.operator = value;
+    } else {
+      operate();
+      expression.operator = value;
+    }
+  }
+  console.log(expression);
+  renderScreen();
+}
+
+function assignNumbersEventListener(buttons) {
+  for (const item of buttons) {
+    item.addEventListener("click", updateExpression);
+  }
+}
+
+//Calls
+
+assignNumbersEventListener(numbers);
+assignNumbersEventListener(operators);
